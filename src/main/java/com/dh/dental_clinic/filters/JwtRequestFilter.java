d4e4e93cb78ffb5ec1f3c055e2c115d890157d5e
DeividGonzalezDev <bienvenidoexperto@gmail.com>
2023-07-07 19:00:34 -0300

    fix Authorization To The h2 console

--- src/main/java/com/dh/dental_clinic/filters/JwtRequestFilter.java
+++ src/main/java/com/dh/dental_clinic/filters/JwtRequestFilter.java
@@ -1,6 +1,7 @@
 package com.dh.dental_clinic.filters;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,16 +15,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.dh.dental_clinic.services.impl.JwtUtilService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-  
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -32,11 +35,48 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtRequestFilter.class);
 
+    public Optional<String> getJWT(HttpServletRequest request) {
+        String cookieHeader = request.getHeader("Cookie");
+        logger.info("cookies: " + cookieHeader);
+        if (cookieHeader != null) {
+            String[] cookies = cookieHeader.split("; ");
+            String jwt;
+
+            for (String cookie : cookies) {
+                if (cookie.startsWith("userData=")) {
+                    String cookieValue = cookie.substring("userData=".length());
+                    int startIndex = cookieValue.indexOf("\"jwt\":\"") + 7;
+                    int endIndex = cookieValue.lastIndexOf("\"}");
+
+                    if (startIndex != -1 && endIndex != -1) {
+                        jwt = cookieValue.substring(startIndex, endIndex);
+                    } else {
+                        throw new IllegalArgumentException("Formato de cookie incorrecto");
+                    }
+                    return jwt == null ?  Optional.empty() : Optional.of(jwt);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
-        final String authorizationHeader = request.getHeader("Authorization");
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (httpRequest.getRequestURI().contains("/h2-console")) {
+            logger.info("h2-console");
+            // logger.info(getJWT(request).get());
+            Optional<String> jwt = getJWT(request);
+            if (jwt.isPresent()) {
+                logger.info("jwtForH2Console: " + jwt.get());
+                authorizationHeader = "Bearer " + jwt.get();
+            }
+        }
 
         String username = null;
         String jwt = null;
@@ -48,7 +88,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             logger.info("context: " + SecurityContextHolder.getContext().getAuthentication());
         }
 
-        //&& SecurityContextHolder.getContext().getAuthentication() == null
+        // && SecurityContextHolder.getContext().getAuthentication() == null
 
         if (username != null) {
 
